d85973e45172f1d4d4bc2b6a9baab44652432373
Radoslaw Sobczak <r.sobczak@onet.eu>
2020-08-02 12:21:28 +0200

    Initial Commit

---
+++ ShoppingList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShoppingList
+//
+//  Created by Radoslaw Sobczak on 02/08/2020.
+//  Copyright © 2020 Radoslaw Sobczak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
